2cd860a0c98d62ffd7fe423c2d8c005f6b739124
Virag Kormoczy <vkormoczy@gmail.com>
2022-12-14 12:54:32 +0200

    Make page assets reusable

--- private/initialize.php
+++ private/initialize.php
@@ -1,5 +1,11 @@
 <?php
 
+// Find this file's location and get its directory
+
+define("PRIVATE_PATH", dirname(__FILE__));
+define("PROJECT_PATH", dirname(PRIVATE_PATH));
+define("PUBLIC_PATH", PROJECT_PATH . '/public');
+define("SHARED_PATH", PRIVATE_PATH . '/shared');
+
 require_once('functions.php');
 
-?>
