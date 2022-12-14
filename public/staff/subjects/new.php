3147b22769e63026a2aaa325e229cf3dbc660e11
Virag Kormoczy <vkormoczy@gmail.com>
2022-12-14 16:55:08 +0200

    Modify headers

---
+++ public/staff/subjects/new.php
@@ -0,0 +1,15 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
+<?php
+
+$test = $_GET['test'] ?? '';
+
+if($test == '404') {
+  error_404()();
+} elseif($test == '500') {
+  error_500();
+} else {
+  echo "No error";
+}
+
+?>
