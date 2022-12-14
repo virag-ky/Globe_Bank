8e5b058077a4655964345256523c7d99ea98a053
Virag Kormoczy <vkormoczy@gmail.com>
2022-12-14 14:55:52 +0200

    Encode URL parameters

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,3 +1,4 @@
+<?php require_once('../../../private/initialize.php'); ?>
 <?php
 
 $id = $_GET['id'] ?? '1';
@@ -5,3 +6,8 @@ $id = $_GET['id'] ?? '1';
 echo $id;
 
 ?>
+
+
+<!-- <a href="show.php?name=<?php echo u('Jane Doe'); ?>">Name</a>
+<a href="show.php?company=<?php echo u('C&C'); ?>">Company</a>
+<a href="show.php?query=<?php echo u('!?#*'); ?>">Link</a> -->
