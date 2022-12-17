0b3cca464f63789f3633bb27a7ee552bd23ce485
Virag Kormoczy <vkormoczy@gmail.com>
2022-12-17 12:56:24 +0200

    Display validation errors

--- private/initialize.php
+++ private/initialize.php
@@ -23,5 +23,6 @@ require_once('query_functions.php');
 require_once('validation_functions.php');
 
 $db = db_connect();
+$errors = [];
 
 ?>
--- public/staff/subjects/edit.php
+++ public/staff/subjects/edit.php
@@ -20,15 +20,16 @@ if(is_post_request()) {
     redirect_to(url_for('/staff/subjects/show.php?id=' . $id));
   } else {
     $errors = $result;
-    var_dump($errors);
+    //var_dump($errors);
   }
 
 } else {
   $subject = find_subject_by_id($id);
-  $subject_set = find_all_subjects();
-  $subject_count = mysqli_num_rows($subject_set);
-  mysqli_free_result($subject_set);
 }
+
+$subject_set = find_all_subjects();
+$subject_count = mysqli_num_rows($subject_set);
+mysqli_free_result($subject_set);
 ?>
 
 <?php $page_title = 'Edit Subject'; ?>
@@ -37,6 +38,7 @@ if(is_post_request()) {
   <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
   <div class="subject edit">
     <h1>Edit Subject</h1>
+    <?php echo display_errors($errors); ?>
     <form action="<?php echo url_for('/staff/subjects/edit.php?id=' . h(u($id))); ?>" method="post">
       <dl>
         <dt>Menu Name</dt>
